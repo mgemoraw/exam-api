5b61f4ca5e144af6f60ad7772a91f155695868f8
mgemoraw <meng.get4@gmail.com>
2026-01-26 01:10:34 +0300

    adding minor updates on user dependency

--- app/core/security.py
+++ app/core/security.py
@@ -16,7 +16,7 @@ from app.core.config import settings
 SECRET_KEY = "NKPD9W4hV/+YStZ+RejELM68Dw5okI5TrYrNWRcIf8q/OGfvxQXvtEirGA4yp9syAQkf3CWFqzH/nrV844dj8Q=="
 ALGORITHM = "HS256"
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login", auto_error=False)
 pwd_context = CryptContext(schemes=['argon2', "bcrypt", "pbkdf2_sha256"], deprecated='auto')
 
 
--- app/schemas/exam.py
+++ app/schemas/exam.py
@@ -2,15 +2,21 @@ from datetime import datetime
 from pydantic import BaseModel
 from typing import Optional
 from uuid import uuid4, UUID
+from enum import Enum
 
 class ExamRequest(BaseModel):
     pass 
+
+class ExamTypeEnum(Enum):
+    MODEL_EXIT_EXAM ="model exit exam"
+    HOLISTIC_EXAM="holistic exam"
+    ENTRANCE_EXAM="entrance exam"
 
 class ExamCreateRequest (BaseModel):
     id: UUID
     duration_minutes: int 
     maximum_marks: int 
-    
+    exam_type: Optional[Enum]=ExamTypeEnum.MODEL_EXIT_EXAM    
     class Config:
         from_attributes=True
 
